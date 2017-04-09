5e2085ca8691d3460d9d32510bac69311e6a09e0
winter <bestlovedream23@gmail.com>
2017-04-09 14:54:46 +0700

    get location ok

--- app/src/main/java/findhosp/com/findhosp/NearActivity.java
+++ app/src/main/java/findhosp/com/findhosp/NearActivity.java
@@ -1,5 +1,12 @@
 package findhosp.com.findhosp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,9 +20,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class NearActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //Exp;icit
     private GoogleMap mMap;
     private int indexAnInt;
     private String tag = "9AprilV2";
+    private double latADouble, longADouble;
+    private LocationManager locationManager;
+    private Criteria criteria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +37,109 @@ public class NearActivity extends FragmentActivity implements OnMapReadyCallback
         //Get Value From Intent
         getValueFromIntent();
 
+        //Setup
+        mySetup();
+
         // create map Fragment
         createFragment();
 
     }   // Main Method
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+
+        latADouble = 13.964591;
+        longADouble = 100.585552;
+
+        //For Find Location by Network
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+
+            latADouble = networkLocation.getLatitude();
+            longADouble = networkLocation.getLongitude();
+
+        }
+
+        //For Find Location my GPS card
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            longADouble = gpsLocation.getLongitude();
+
+        }
+        Log.d(tag, "Lat ==> " + latADouble);
+        Log.d(tag, "Lng ==> " + longADouble);
+
+    }   //onResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latADouble = location.getLatitude();
+            longADouble = location.getLongitude();
+
+
+        }   //onLocationChanged
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void mySetup() {
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+    }
 
     private void getValueFromIntent() {
         indexAnInt = getIntent().getIntExtra("Index", 0);
